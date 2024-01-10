e8c6aa769583dab7081214081d16b7b92e0bf74b
JuanJoAraujo2003 <juan.araujo.bedon@udla.edu.ec>
2024-01-10 07:39:54 -0500

    Agregar archivos de proyecto.

---
+++ Araujo_EjemploAPI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Araujo_EjemploAPI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Araujo_EjemploAPI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Araujo_EjemploAPI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
